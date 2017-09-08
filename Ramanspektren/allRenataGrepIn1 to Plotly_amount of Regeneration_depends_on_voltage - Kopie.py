f87683e83fbdfb92fdc50c7a0fc5692dd26bc646
ehoehn <69480546+ehoehn@users.noreply.github.com>
2017-09-08 11:52:06 +0200

    plotly_y_dependent_of_x zu lib hinzugefügt

---
+++ Ramanspektren/allRenataGrepIn1 to Plotly_amount of Regeneration_depends_on_voltage - Kopie.py
@@ -0,0 +1,51 @@
+import os
+import pandas as pd
+from lib.allgemein import liste_in_floats_umwandeln
+from lib.xml_import import get_times
+from lib.plotlygraphen import plotly_y_dependent_of_x
+
+
+suffix_for_new_filename = '_RegenerationVsVoltage.html'
+
+
+'''
+imput file: eine .csv-Datei Datei mit zeitl Verlauf in einer Tabelle nach baseline korrektur
+output file: schlichter Graph mit 2D-Scatterplot in Plotly
+
+'''
+
+for dateiname in os.listdir():
+    if dateiname.endswith('_beiF1Analyt_beiF20ElektrolythFlussAn_beiF200ProgStart.tvf') or dateiname.endswith('_beiF1Analyt_beiF20ElektrolythFlussAn_beiF200ProgStart.TVF'):
+     #   print(dateiname)
+        times = get_times(dateiname)
+
+        TimeVoltageOn = round(times['Frame 200']['time [s]'] + 100, 0)
+        FrameVoltageOn = times[times.columns[times.ix['time [s]'] > TimeVoltageOn - 1]].columns[0]
+# break
+
+for dateiname in os.listdir():
+    if dateiname.endswith('usammenfassung_Renata_grep.csv') or dateiname.endswith('usammenfassung_Renata_grep.CSV'):
+        print(dateiname)
+        with open(dateiname) as fd:
+            df = pd.read_csv(fd, index_col=0, header=0, sep=';')
+            df2 = df.apply(lambda x: x / df[FrameVoltageOn] * 100, axis=0)  # Normalisierung
+
+            voltage = []
+            for k in df2.index:
+                nr = k.split('_')
+                nr = nr[6]
+                nr = nr[:-1]
+                nr = nr.replace(',', '.')
+                voltage.append(nr)
+            x_values = voltage
+            x_values = liste_in_floats_umwandeln(x_values)
+           # print(x_values)
+
+            interval = df2.ix[:, FrameVoltageOn:]
+            y_values = 100 - interval.min(axis=1)
+        #    print(y_values)
+
+            plotly_y_dependent_of_x(x_values, y_values, dateiname, suffix_for_new_filename,
+                                    x_range=None, y_range=[0, 105],
+                                    x_dtick=2.5, y_dtick=10,
+                                    xaxis_title='U [V]', yaxis_title='signal decrease [%]')
--- Ramanspektren/lib/plotlygraphen.py
+++ Ramanspektren/lib/plotlygraphen.py
@@ -1,5 +1,7 @@
 import plotly
 from plotly import graph_objs as go
+from lib.allgemein import generate_filename
+
 
 grenzfarbe = 0.7
 schritte = 20 - 1
@@ -7,6 +9,11 @@ schritte = 20 - 1
 
 def viridis_plus_rot():
     farbe = [[grenzfarbe - grenzfarbe / schritte * 19, '#440154FF'], [grenzfarbe - grenzfarbe / schritte * 18, '#481567FF'], [grenzfarbe - grenzfarbe / schritte * 17, '#482677FF'], [grenzfarbe - grenzfarbe / schritte * 16, '#453781FF'], [grenzfarbe - grenzfarbe / schritte * 15, '#404788FF'], [grenzfarbe - grenzfarbe / schritte * 14, '#39568CFF'], [grenzfarbe - grenzfarbe / schritte * 13, '#33638DFF'], [grenzfarbe - grenzfarbe / schritte * 12, '#2D708EFF'], [grenzfarbe - grenzfarbe / schritte * 11, '#287D8EFF'], [grenzfarbe - grenzfarbe / schritte * 10, '#238A8DFF'], [grenzfarbe - grenzfarbe / schritte * 9, '#1F968BFF'], [grenzfarbe - grenzfarbe / schritte * 8, '#20A387FF'], [grenzfarbe - grenzfarbe / schritte * 7, '#29AF7FFF'], [grenzfarbe - grenzfarbe / schritte * 6, '#3CBB75FF'], [grenzfarbe - grenzfarbe / schritte * 5, '#55C667FF'], [grenzfarbe - grenzfarbe / schritte * 4, '#73D055FF'], [grenzfarbe - grenzfarbe / schritte * 3, '#95D840FF'], [grenzfarbe - grenzfarbe / schritte * 2, '#B8DE29FF'], [grenzfarbe - grenzfarbe / schritte * 1, '#DCE319FF'], [grenzfarbe, '#FDE725FF'], [1, '#B20A28FF']]
+    return farbe
+
+
+def viridis_plus_rot_as_list():
+    farbe = ['#440154FF', '#481567FF', '#482677FF', '#453781FF', '#404788FF', '#39568CFF', '#33638DFF', '#2D708EFF', '#287D8EFF', '#238A8DFF', '#1F968BFF', '#20A387FF', '#29AF7FFF', '#3CBB75FF', '#55C667FF', '#73D055FF', '#95D840FF', '#B8DE29FF', '#DCE319FF', '#FDE725FF', '#B20A28FF']
     return farbe
 
 
@@ -212,6 +219,85 @@ def plotly_Spectrum_2dscatter_layout(ind, xaxis_title, yaxis_title, range_nr, dt
     return layout
 
 
-def viridis_plus_rot_as_list():
-    farbe = ['#440154FF', '#481567FF', '#482677FF', '#453781FF', '#404788FF', '#39568CFF', '#33638DFF', '#2D708EFF', '#287D8EFF', '#238A8DFF', '#1F968BFF', '#20A387FF', '#29AF7FFF', '#3CBB75FF', '#55C667FF', '#73D055FF', '#95D840FF', '#B8DE29FF', '#DCE319FF', '#FDE725FF', '#B20A28FF']
-    return farbe
+def plotly_y_dependent_of_x_2dscatter_data(x_values, y_values):
+    print(plotly.__version__)
+    trace = go.Scatter(
+        x=x_values,
+        y=y_values,
+        mode='markers',
+        marker=dict(
+            sizemode='diameter',
+            sizeref=1,  #relative Größe der Marker
+            sizemin=20,
+            size=10,
+            color='#000000',
+            opacity=0.8,
+            line=dict(color='rgb(166, 166, 166)',
+                      width=0)))
+    data = [trace]
+    return data
+
+
+def plotly_y_dependent_of_x_2dscatter_layout(xaxis_title, yaxis_title, x_range=None, y_range=None, x_dtick=None, y_dtick=None):
+    #data = [trace]
+    layout = go.Layout(
+        autosize=False,
+        width=800,
+        height=430,
+        # title='<b>Titel</b>',
+        # titlefont=dict(family='Arial, sans-serif',
+        #                size=14,
+        #                color='#000000'),
+        xaxis=dict(
+            title='<b>' + xaxis_title + '</b>',
+            titlefont=dict(family='Arial bold, sans-serif',
+                           size=20,
+                           color='#000000'),
+            showticklabels=True,
+            tickangle=0,
+            tickfont=dict(family='Arial, sans-serif',
+                          size=20,
+                          color='#000000'),
+            showgrid=False,
+            showline=True,
+            linewidth=2,
+            autotick=False,
+            ticks='outside',
+            tick0=0,
+            ticklen=5,
+            tickwidth=1,
+            tickcolor='#FFFFFF',
+            range=x_range,
+            #   range=[0, 2.5],
+            dtick=x_dtick),
+        yaxis=dict(
+            title='<b>' + yaxis_title + '</b>',
+            titlefont=dict(family='Arial bold, sans-serif',
+                           size=20,
+                           color='#000000'),
+            showticklabels=True,
+            tickangle=0,
+            tickfont=dict(family='Arial, sans-serif',
+                          size=20,
+                          color='#000000'),
+            showgrid=True,
+            gridwidth=1,
+            gridcolor='#8F8F8F',
+            showline=True,
+            linewidth=2,
+            autotick=False,
+            ticks='outside',
+            tick0=0,
+            ticklen=5,
+            tickwidth=1,
+            tickcolor='#FFFFFF',
+            range=y_range,
+            # range=[0, 105],
+            dtick=y_dtick))
+    return layout
+
+
+def plotly_y_dependent_of_x(x_values, y_values, dateiname, suffix_for_new_filename, x_range, y_range, x_dtick, y_dtick, xaxis_title, yaxis_title):
+    nwfile = generate_filename(dateiname, suffix_for_new_filename)
+    fig = dict(data=plotly_y_dependent_of_x_2dscatter_data(x_values, y_values), layout=plotly_y_dependent_of_x_2dscatter_layout(xaxis_title, yaxis_title, x_range, y_range, x_dtick, y_dtick))
+    plotly.offline.plot(fig, filename=nwfile,  image_filename=nwfile)  #, image='png', image_width=1600, image_height=860)
