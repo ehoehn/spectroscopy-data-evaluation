2fe95417621291a436fe894cef3f638361f24cd9
ehoehn <69480546+ehoehn@users.noreply.github.com>
2017-09-28 15:01:15 +0200

    angepasst auf Spannung an bei Frame 200 und Spannung aus bei Frame 300

--- Ramanspektren/allRenataGrepIn1 Normalisierung - Kopie mit Interation.py
+++ Ramanspektren/allRenataGrepIn1 Normalisierung - Kopie mit Interation.py
@@ -4,7 +4,7 @@ import plotly
 import plotly.graph_objs as go  # import Scatter, Layout
 from Ramanspektren.lib.allgemein import generate_filename
 from Ramanspektren.lib.plotlygraphen import plotly_zeitlVerlauf_2dscatter_layout
-from Ramanspektren.lib.plotlygraphen import viridis_plus_rot_as_list, cmocean_gray
+from Ramanspektren.lib.plotlygraphen import viridis_plus_rot_as_list
 from Ramanspektren.lib.xml_import import get_times
 
 
@@ -34,7 +34,7 @@ def plotly_zeitlVerlauf_2dscatter_data(highest_intensity):
             x=ind,
             y=nrCol[t],
             mode='lines',
-            line=go.Line(color=viridis_plus_rot_as_list()[t], width=3),
+            line=go.Line(color=viridis_plus_rot_as_list()[t*2], width=3),
             name=voltage[t],
             showlegend=True)
         traces.append(trace)
@@ -49,13 +49,13 @@ def plotly_zeitlVerlauf(df, dateiname, suffix_for_new_filename, xaxis_title, yax
 
 
 
-for dateiname in os.listdir():
-    if dateiname.endswith('_beiF1Analyt_beiF20ElektrolythFlussAn_beiF200ProgStart.tvf') or dateiname.endswith('_beiF1Analyt_beiF20ElektrolythFlussAn_beiF200ProgStart.TVF'):
-     #   print(dateiname)
-        times = get_times(dateiname)
-
-        TimeVoltageOn = round(times['Frame 200']['time [s]'] + 100, 0)
-        FrameVoltageOn = times[times.columns[times.ix['time [s]'] > TimeVoltageOn - 1]].columns[0]
+# for dateiname in os.listdir():
+#     if dateiname.endswith('_beiF1Analyt_beiF20ElektrolythFlussAn_beiF200ProgStart.tvf') or dateiname.endswith('_beiF1Analyt_beiF20ElektrolythFlussAn_beiF200ProgStart.TVF'):
+#      #   print(dateiname)
+#         times = get_times(dateiname)
+#
+#         TimeVoltageOn = round(times['Frame 200']['time [s]'] + 100, 0)
+#         FrameVoltageOn = times[times.columns[times.ix['time [s]'] > TimeVoltageOn - 1]].columns[0]
 
 
 for dateiname in os.listdir():
@@ -63,6 +63,6 @@ for dateiname in os.listdir():
         print(dateiname)
         with open(dateiname) as fd:
             df = pd.read_csv(fd, index_col=0, header=0, sep=';')
-            df2 = df.apply(lambda x: x / df[FrameVoltageOn] * 100, axis=0)  # Normalisierung
+            df2 = df.apply(lambda x: x / df['Frame 200'] * 100, axis=0)  # Normalisierung
 
             plotly_zeitlVerlauf(df2, dateiname, suffix_for_new_filename, xaxis_title='Frame', yaxis_title='Intensity [a. u.]')
